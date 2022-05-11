983425b96cc70d610c93deb7701dc40ce47be72d
HADO564 <70658014+HADO564@users.noreply.github.com>
2022-05-11 18:05:20 +0500

    Finished on the authorization route. Esp32 will request the server if the device that connected is authorized or not. Server will send respective response.

--- routes/router.js
+++ routes/router.js
@@ -3,28 +3,35 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-router.post("/:mac_add", validateUser);
+router.post("/:id", validateUser);
 router.get("/",reply);
 // console.log(prism);
 async function validateUser(req, res) {
     try{
-    const mac_add = req.params.mac_add;   // get mac_address from url
-    const user = await prisma.addresses.findOne({
+    const id = req.params.id;   // get mac_address from url
+    const user = await prisma.addresses.findUnique({//query to check if a mac address is authorized
         where:
         {
-            mac_add: addresses.mac_add
+            mac_add: id
     }});
+    if( user!=null)//if the mac address is authorized
+    {
     res.send("Valid");
     console.log("Yes response");
-} catch(err){
-    res.send("Invalid");
+    }
+    else//if the mac address is unauthorized
+    {
+        res.send("Invalid");
+    }
+} catch(err){//in case there is an error
+    res.send(err);
     console.log("No response");
 
 }
 }
 
-async function reply(req,res){
-    res.send("Route is working, Hoppscotch isnt");
+async function reply(req,res){//testing function for routes
+    res.send("Route is working);
 }
 
 module.exports = router;
