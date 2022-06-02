a58b96b7417e12a2b62eb52ef77776450d55c1b4
HADO564 <70658014+HADO564@users.noreply.github.com>
2022-06-02 13:55:30 +0500

    Final touches. Server logs data

--- routes/router.js
+++ routes/router.js
@@ -3,8 +3,35 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+
+function log_date(){
+let date_ob = new Date();
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds();
+
+// prints date in YYYY-MM-DD format
+console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+}
+
 router.get("/:id", validateUser);
-router.post("/addAddress/:id", addAddress);//in case i decide to make an online app for registration of mac addresses
+//router.post("/addAddress/:id", addAddress);//in case i decide to make an online app for registration of mac addresses
 router.get("/",reply);
 // console.log(prism);
 async function validateUser(req, res) {
@@ -17,12 +44,15 @@ async function validateUser(req, res) {
     }});
     if( user!=null)//if the mac address is authorized
     {
-    res.send("Valid");
-    console.log("Yes response");
+    console.log("Access Granted at: ")
+    log_date();
+    console.log("To mac address :")
+    console.log(id);
+    res.send("valid");
     }
     else//if the mac address is unauthorized
     {
-        res.send("Invalid");
+        res.send("invalid");
     }
 } catch(err){//in case there is an error
     res.send(err);
